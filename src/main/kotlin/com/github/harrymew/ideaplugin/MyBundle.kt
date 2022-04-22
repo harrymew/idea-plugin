2402ffac39b7633e6b6cfb5de80e576b310b58ae
GitHub Action <action@github.com>
2022-04-22 07:09:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/harrymew/ideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/harrymew/ideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.harrymew.ideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/harrymew/ideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/harrymew/ideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.harrymew.ideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.harrymew.ideaplugin.MyBundle
 
 class MyApplicationService {
 
